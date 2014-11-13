92e7b067a287dd96f9740f7ad3a3baea96f9a383
Christian Strand Young <christian@strandyoung.com>
2014-11-13 21:36:30 +0100

    Made a simple shop on stuff

---
+++ products/order.js
@@ -0,0 +1,38 @@
+var items = [
+		{
+			'name' : 'Galhøi',
+			'price' : 4500
+		},
+		{
+			'name' : 'Snøhetta',
+			'price' : 6800
+		},
+		{
+			'name' : 'Storebjørn',
+			'price' : 4300
+		},
+		{
+			'name' : 'Trolltind',
+			'price' : 4700
+		},
+		{
+			'name' : 'Veotind',
+			'price' : 12300
+		},
+		{
+			'name' : 'Veslebjørn',
+			'price' : 9900
+		},
+	];
+
+$(function () {
+
+	$('.buy').on('click', function (e) {
+		e.preventDefault();
+		var id = $(this).data('id');
+		$('.item-name').text(items[id]['name']);
+		$('.item-value').text(items[id]['price']);
+		$('.total').text((parseInt(items[id]['price']) + 300));
+		$('#buyModal').modal();
+	});
+});
